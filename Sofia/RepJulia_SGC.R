f773c950879b4ee7c7f7aece27b8a4c87baa102c
Sofia Gil-Clavel <gil@demogr.mpg.de>
2021-10-04 21:55:07 +0200

    Update

--- Sofia/RepJulia_SGC.R
+++ Sofia/RepJulia_SGC.R
@@ -21,8 +21,8 @@ DATA<-DATA%>%select("DHHGAGE","DHH_SEX","DHHGMS","EDUDR04","NSIDSC","GENGSWL")
 #### Variable transformations ####
 # DHH_SEX: MALE(1) & FEMALE(2)
 DATA$DHH_SEX<-factor(DATA$DHH_SEX,
-               levels = c(1,2),
-               labels = c("MALE","FEMALE"))
+                     levels = c(1,2),
+                     labels = c("MALE","FEMALE"))
 
 # DHHGAGE: Age - grouped variable
 A1<-seq(15,80,5)
@@ -30,17 +30,20 @@ A2<-A1+4; A2[length(A2)]="more"
 Alabels<-paste0(A1,"-",A2)
 
 DATA$DHHGAGE<-factor(DATA$DHHGAGE,
-                    levels = 1:14,
-                    labels = Alabels)
+                     levels = 1:14,
+                     labels = Alabels)
 
 # DHHGMS: MARRIED(1), COMMON-LAW(2), WIDOWED(3), DIVORCED/SEPARATED(4)
 #         SINGLE(5), NOT STATED(9)
 DHHGMS<-c("MARRIED", "COMMON-LAW", "WIDOWED", "DIVORCED/SEPARATED",
-          "SINGLE", NA)
+          "SINGLE")
 #** NOTE: Transform NOT STATED into NA
 
+INDEX=which(DATA$DHHGMS==9)
+DATA$DHHGMS[INDEX]=NA
+
 DATA$DHHGMS<-factor(DATA$DHHGMS,
-                    levels = c(1:5,9),
+                    levels = c(1:5),
                     labels = DHHGMS)
 
 # EDUDH04: Education
@@ -49,13 +52,16 @@ DATA$DHHGMS<-factor(DATA$DHHGMS,
 # SOME POST-SECONDARY(3)
 # POST-SECONDARY GRADUATION(4)
 # NOT STATED(9)
-EDUDR04<-c("LessSec.School","Sec.School","Some Post.Sec.","Post.Sec.",NA)
+EDUDR04<-c("LessSec.School","Sec.School","Some Post.Sec.","Post.Sec.")
 
 #** NOTE: Transform NOT STATED into NA
 
+INDEX=which(DATA$EDUDR04==9)
+DATA$EDUDR04[INDEX]=NA
+
 DATA$EDUDR04<-factor(DATA$EDUDR04,
-                    levels = c(1:4,9),
-                    labels = EDUDR04)
+                     levels = c(1:4),
+                     labels = EDUDR04)
 
 # NSIDSC (continuous): NEGATIVE SOCIAL INTERACTIONS SCALE 
 #** NOTE: Transform NOT STATED into NA
@@ -63,8 +69,7 @@ INDEX=which(DATA$NSIDSC==99)
 DATA$NSIDSC[INDEX]=NA
 
 # GENGSWL: Satisfaction with life in general
-GENGSWL<-c("VERY SATISFIED", "SATISFIED", "NEITHER SATISFIED NOR DISATISFIED", 
-          "DISSATISFIED","VERY DISSATISFIED")
+GENGSWL<-c("VERY SATISFIED", "SATISFIED", "NEITHER SATISFIED NOR DISATISFIED", "DISSATISFIED","VERY DISSATISFIED")
 
 #** NOTE: Transform NOT STATED into NA
 INDEX=which(DATA$GENGSWL==9)
@@ -75,13 +80,8 @@ DATA$GENGSWL<-abs(DATA$GENGSWL-5)+1
 DATA$GENGSWL[INDEX]=NA
 
 DATA$GENGSWL2<-factor(DATA$GENGSWL,
-                    levels = c(5:1),
-                    labels = GENGSWL)
-
-hist(DATA$GENGSWL)
-
-# Checking the Variables
-summary(DATA)
+                      levels = c(5:1),
+                      labels = GENGSWL)
 
 #### Define Analytic Sample ####
 DATA<-DATA%>%filter(DHHGAGE%in%Alabels[10:14])
